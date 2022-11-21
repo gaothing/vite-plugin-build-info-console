e767bdbcb8cb9a3831fef5c13232cd8026b87394
gaothing <744989745@qq.com>
2022-11-21 12:12:48 +0800

    add

---
+++ README.md
@@ -0,0 +1,15 @@
+# vite-plugin-build-info-console
+
+打印项目打包（构建）时间，可用来判断当前项目访问是否已更新
+
+使用案例
+```js
+vite.config.js
+export default defineConfig({
+  plugins: [
+   //...
+    vitePluginBuild({output:'dist'}), output//默认为dist，文件输出目录
+   
+  ]
+})
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+
+import fs from 'fs';
+import path from 'path';
+
+export default (options) => {
+  const { output } = options;
+  let env;
+  return {
+    name:'vite-plugin-build-info-console',
+    config(_, { command }) {
+      env = command;
+    },
+    transformIndexHtml(result) {
+      console.log({ env })
+      if (env!=="serve") {
+        
+      const dateString = new Date().toLocaleString();
+      const windowConf = `window.__APP__CONFIG__`;
+      const configStr = `${windowConf}=${JSON.stringify(options)};
+      Object.freeze(${windowConf});
+      Object.defineProperty(window, "__APP__CONFIG__", {
+        configurable: false,
+        writable: false,
+      });
+      console.log('%c 项目构建时间:','color:red');
+      console.log('%c ${dateString}','color:#1D63F2');
+    `.replace(/\s/g, '');
+      fs.writeFileSync(getRootPath(`${output??'dist'}/__app__config__.js`), configStr);
+      result = result.replace(/<\/html>/, `<script src="./__app__config__.js?${dateString}"></script>\n</html>`);
+      }
+        
+      return result;
+    }
+  }
+};
+/**
+ * Get user root directory
+ * @param dir file path
+ */
+  function getRootPath(...dir) {
+  return path.resolve(process.cwd(), ...dir);
+}
+
